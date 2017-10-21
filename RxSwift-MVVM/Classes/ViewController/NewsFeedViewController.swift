5bf2b9b5f90805bbcee318617cdb7896b278a598
Khemmachart Chutapetch <nueve.sr@gmail.com>
2017-10-21 12:36:48 +0700

    Set NewsFeed as a initialize view controller

---
+++ RxSwift-MVVM/Classes/ViewController/NewsFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewsFeedViewController.swift
+//  RxSwift-MVVM
+//
+//  Created by Khemmachart Chutapetch on 10/21/2560 BE.
+//  Copyright © 2560 Khemmachart Chutapetch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+    
+}
---
+++ RxSwift-MVVM/Classes/ViewModel/NewsFeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewsFeedViewModel.swift
+//  RxSwift-MVVM
+//
+//  Created by Khemmachart Chutapetch on 10/21/2560 BE.
+//  Copyright © 2560 Khemmachart Chutapetch. All rights reserved.
+//
+
+import Foundation
+
+class NewsFeedViewModel {
+    
+}
